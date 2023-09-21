c754a13ee5f9998238a9d75d444752b5c9ee2c87
Ritika Kumar <kumarritika666@gmail.com>
2023-09-21 21:13:30 +1400

    second commit

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-var ctx = document.getElementById("mycanvas").getContext("2d");
+// var ctx = document.getElementById("mycanvas").getContext("2d");
 
-var mychart = new Chart(ctx, {
-  type: "bar",
-  data: {
-    labels: ["python", "js", "java", "php", "c++", "c#"],
-    datasets: [
-      {
-        label: "Number of Developers",
-        data: [13, 15, 5, 9, 10, 8],
-        backgroundColor: ["red", "blue", "green", "yellow", "orange", "purple"], // Define colors for bars
-      },
+// var mychart = new Chart(ctx, {
+//   type: "bar",
+//   data: {
+//     labels: ["python", "js", "java", "php", "c++", "c#"],
+//     datasets: [
+//       {
+//         label: "Number of Developers",
+//         data: [13, 15, 5, 9, 10, 8],
+//         backgroundColor: ["red", "blue", "green", "yellow", "orange", "purple"], // Define colors for bars
+//       },
 
-      {
-        data: [10, 5, 15, 5, 4, 7],
-      },
-    ],
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true,
-      },
-    },
-  },
-});
+//       {
+//         ,
+//       },
+//     ],
+//   },
+//   options: {
+//     scales: {
+//       y: {
+//         beginAtZero: true,
+//       },
+//     },
+//   },
+// });
